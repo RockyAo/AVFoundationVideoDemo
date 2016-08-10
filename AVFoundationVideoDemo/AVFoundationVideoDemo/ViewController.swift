d6020b3d76916d08cb37856cf3c2118e24baf41b
Rocky Ao <542622608@qq.com>
2016-08-10 12:10:46 +0800

    初始化项目

---
+++ AVFoundationVideoDemo/AVFoundationVideoDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AVFoundationVideoDemo
+//
+//  Created by ZCBL on 16/8/10.
+//  Copyright © 2016年 RA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
